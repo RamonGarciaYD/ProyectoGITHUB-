b1ee95719267b9e89bbec3e9eb15558409943594
RamonYMarcos <ramonfpb02@gmail.com>
2024-04-08 12:24:36 +0200

    Este es el segundo commit de la clase 1

--- src/eclipsegit/Clase1.java
+++ src/eclipsegit/Clase1.java
@@ -4,6 +4,8 @@ public class Clase1 {
 
 	public static void main(String[] args) {
 	 System.out.println("Hola mundo, este es mi primer commit desde eclipse");
+	 System.out.println("Hola mundo, este es mi segundo commit desde eclipse");
+
 	}
 
 }
